28efd605ede156f5896f48f1efd74d336351f1ae
PSE20 <pse20@z031enfp-9204.infra.enc>
2018-04-25 12:02:40 +0200

    Ajout font + menu html css + fichiers vues + logo

---
+++ WEB/view/header.view.php
@@ -0,0 +1,21 @@
+<nav class="navbar fixed-top navbar-expand-sm" id="headerDef">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarBet" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">
+    <img id="logoBetFip" src="../ressource/img/Logo_BetFip.png" height="90" alt="logo BetFiP">
+  </a>
+  <div class=" navbar-collapse collapse container-fluid" id="navbarBet">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link selected lien" href="#">Paris</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link lien" href="#">Classement</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      <a href="#" class="nav-link lien"><span class="glyphicon glyphicon-user "></span>LaBedaine</a>
+    </span>
+  </div>
+</nav>
