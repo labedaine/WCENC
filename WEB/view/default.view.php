2189f157b69fdc31d8a86d08a4429f7b66c4853a
PSE20 <pse20@z031enfp-9204.infra.enc>
2018-04-27 09:21:14 +0200

    Modif pour pages connexion et register + css et js local sur default page

--- WEB/view/default.view.php
+++ WEB/view/default.view.php
@@ -2,14 +2,11 @@
 <html lang='fr'>
   <head>
     <link rel="stylesheet" href="../ressource/css/style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="../commun/css/bootstrap/dist/css/bootstrap.min.css">
+    <script src="../commun/js/jquery.min.js"></script>
+    <script src="../commun/css/bootstrap/dist/js/bootstrap.min.css"></script>
   </head>
-  <body>
-    <?php include "header.view.php"; ?>
-    <div id="content" class="container-fluid">
-
-    </div>
+  <body class="text-center">
+        <?php include "inscription.view.php"; ?>
   </body>
 </html>
