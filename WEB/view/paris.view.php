38fb0c0b2be824a65ce923c2cb5074b21fa24de8
PSE20 <pse20@z031enfp-9204.infra.enc>
2018-04-27 11:49:49 +0200

    modif view + css

---
+++ WEB/view/paris.view.php
@@ -0,0 +1,62 @@
+<div class="row">
+  <div class="col-2 vertical-menu">
+    <ul class="navbar-nav">
+      <li class="activeGroupe"><a href="#">Groupe A</a></li>
+      <li><a href="#">Groupe B</a></li>
+      <li><a href="#">Groupe C</a></li>
+      <li><a href="#">Groupe D</a></li>
+      <li><a href="#">Groupe E</a></li>
+      <li><a href="#">Groupe F</a></li>
+      <li><a href="#">Groupe G</a></li>
+    </ul>
+  </div>
+  <div class="col-10">
+    <h3>Groupe A</h3>
+    <table id="tabParis" class="table table-hover">
+      <tbody>
+        <tr>
+          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
+          <td>France</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>-</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>Brésil</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
+          <th>Mercredi 3 Avril à 16h</th>
+        </tr>
+        <tr>
+          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
+          <td>France</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>-</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>Brésil</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
+          <th>Mercredi 3 Avril à 16h</th>
+        </tr>
+        <tr>
+          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
+          <td>France</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>-</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>Brésil</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
+          <th>Mercredi 3 Avril à 16h</th>
+        </tr>
+        <tr>
+          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
+          <td>France</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>-</td>
+          <td><input type="number" name="idequipe_match" min="0"></td>
+          <td>Brésil</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
+          <th>Mercredi 3 Avril à 16h</th>
+        </tr>
+      </tbody>
+
+
+    </table>
+  </div>
+</div>
