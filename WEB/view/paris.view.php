88983a3f9e30412237194bdbeb970a71d3426832
PSE20 <pse20@z031enfp-9204.infra.enc>
2018-05-02 12:04:25 +0200

    modif page paris

--- WEB/view/paris.view.php
+++ WEB/view/paris.view.php
@@ -1,5 +1,6 @@
 <div class="row">
   <div class="col-2 vertical-menu">
+    <div class="titleGroupe">Liste des groupes</div>
     <ul class="navbar-nav">
       <li class="activeGroupe"><a href="#">Groupe A</a></li>
       <li><a href="#">Groupe B</a></li>
@@ -11,49 +12,54 @@
     </ul>
   </div>
   <div class="col-10">
-    <h3>Groupe A</h3>
-    <table id="tabParis" class="table table-hover">
+    <h3  class="titlePage">Groupe A</h3>
+    <table id="tabParis" class="table table-hover table-sm no-gutter">
       <tbody>
         <tr>
           <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
-          <td>France</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>-</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>Brésil</td>
+          <td class="col-md-3">France</td>
+          <td class="paris">1</td>
+          <td class="resultat"><span class="rounded-circle">1</span>-<span class="rounded-circle">2</span></td>
+          <td class="paris">1</td>
+          <td class="col-md-3">Brésil</td>
           <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
-          <th>Mercredi 3 Avril à 16h</th>
+          <td class="dateMatch">Mercredi 3 Avril à 16h</td>
+          <th class="pointGagner perdu"></th>
         </tr>
         <tr>
-          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
-          <td>France</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>-</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>Brésil</td>
-          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
-          <th>Mercredi 3 Avril à 16h</th>
+          <td><img src="../ressource/img/drapeaux/drapeau-belgique.png"/></td>
+          <td>Belgique</td>
+          <td class="paris">3</td>
+          <td class="resultat"><span class="rounded-circle">4</span>-<span class="rounded-circle">1</span></td>
+          <td class="paris">1</td>
+          <td>Russie</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-russie.png"/></td>
+          <td class="dateMatch" >Mercredi 3 Avril à 16h</td>
+          <th class="pointGagner perdu"><div class="coin bronze"><p>3</p></div></th>
         </tr>
         <tr>
-          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
-          <td>France</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>-</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>Brésil</td>
-          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
-          <th>Mercredi 3 Avril à 16h</th>
+          <td><img src="../ressource/img/drapeaux/drapeau-portugal.png"/></td>
+          <td>Portugal</td>
+          <td class="paris">1</td>
+          <td class="resultat"><span class="rounded-circle">2</span>-<span class="rounded-circle">2</span></td>
+          <td class="paris">1</td>
+          <td>Espagne</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-espagne.png"/></td>
+          <td class="dateMatch" >Mercredi 3 Avril à 16h</td>
+          <th class="pointGagner perdu"><div class="coin silver"><p>5</p></div></th>
         </tr>
-        <tr>
-          <td><img src="../ressource/img/drapeaux/drapeau-france.png"/></td>
-          <td>France</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>-</td>
-          <td><input type="number" name="idequipe_match" min="0"></td>
-          <td>Brésil</td>
-          <td><img src="../ressource/img/drapeaux/drapeau-bresil.png"/></td>
-          <th>Mercredi 3 Avril à 16h</th>
+        <tr class="success">
+          <td><img src="../ressource/img/drapeaux/drapeau-allemagne.png"/></td>
+          <td>Allemagne</td>
+          <td class="paris">3</td>
+          <td class="resultat"><span class="rounded-circle">3</span>-<span class="rounded-circle">1</span></td>
+          <td class="paris">1</td>
+          <td>Argentine</td>
+          <td><img src="../ressource/img/drapeaux/drapeau-argentine.png"/></td>
+          <td class="dateMatch" >Mercredi 3 Avril à 16h</td>
+          <th class="pointGagner perdu"><div class="coin gold"><p>8</p></div></th>
         </tr>
+
       </tbody>
 
 
